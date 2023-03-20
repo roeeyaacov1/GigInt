73779da75dddd0707507eb459f2d8b4f0e790758
Roee <>
2023-03-21 00:26:40 +0200

    added the base of UserViewSet

--- users/views.py
+++ users/views.py
@@ -1,8 +1,28 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from users.serializers import UserSerializer
+from rest_framework import viewsets
+from .models import User
 
 
 # Create your views here.
-def users(request):
-    return HttpResponse("Hello, world. You're at the users index.")
 
+class UserViewSet(viewsets.ViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+    def create(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    def list(self, request, *args, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        pass
