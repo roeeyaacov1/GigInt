a135fe3550b3a71ab29dbae85fb40dd17f93dedd
Roee <>
2023-03-21 00:19:44 +0200

    added the base of FoodViewSet

--- food/views.py
+++ food/views.py
@@ -1,8 +1,29 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from food.serializers import FoodSerializer
+from rest_framework import viewsets
+from rest_framework.response import Response
+from .models import Food
 
 
 # Create your views here.
-def food(request):
-    return HttpResponse("Hello, world. You're at the foods index.")
 
+class FoodViewSet(viewsets.ViewSet):
+    queryset = Food.objects.all()
+    serializer_class = FoodSerializer
+
+    def create(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    def list(self, request, *args, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        pass
