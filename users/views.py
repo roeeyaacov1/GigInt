779e9e3f7a9d6d1b39f308bc9ab20f19e51d9fba
Start <yaacov5214@gmail.com>
2023-03-22 11:41:04 +0200

    added logger

--- users/views.py
+++ users/views.py
@@ -3,6 +3,10 @@ from django.contrib.auth.models import User
 from rest_framework import viewsets
 from rest_framework.decorators import action
 from rest_framework.response import Response
+
+from Gigint.settings import logger
+from reviews.Serializer import ReviewSerializer
+from reviews.models import Review
 from users.serializers import UserSerializer
 
 
@@ -12,9 +16,15 @@ class UserViewSet(viewsets.ModelViewSet):
     serializer_class = UserSerializer
 
     @action(detail=True, methods=['get'])
-    def get_user_summary(self, username):
-        user = get_object_or_404(User, username=username)
+    def get_user_summary(self, request, pk=None):
+        user = get_object_or_404(User, id=pk)
         serializer = self.get_serializer(user)
         data = serializer.data
+        logger.info(f"User:  {user}")
+
+        reviews = Review.objects.all() # filter(user=user)
+        review_serializer = ReviewSerializer(reviews)
+        data += review_serializer.data
+        logger.info(f"Reviews:  {review_serializer.data}")
 
         return Response(data)
