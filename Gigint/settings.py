251a2c5c02dba2cc98da9b23a9b7bfeb85277b63
Start <yaacov5214@gmail.com>
2023-04-17 14:26:11 +0300

    added allowed origins

--- Gigint/settings.py
+++ Gigint/settings.py
@@ -127,6 +127,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://localhost:8080",
+    "http://127.0.0.1:9000",
+]
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
